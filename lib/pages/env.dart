69c9dab2a99e809e457df702cb4ae3eabbad281c
muhammed almanie <muhammedalmanie@gmail.com>
2022-08-03 16:18:45 +0300

    edit update

---
+++ lib/pages/env.dart
@@ -0,0 +1,5 @@
+class Environment {
+  static final String CLIENT_ID = 'flutter-client';
+  static final String SSO_URL =
+      'http://localhost:8080/auth/realms/matp-flutter';
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,158 @@
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   static const String _title = 'Sample App';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: _title,
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text(_title)),
+//         body: const MyStatefulWidget(),
+//       ),
+//     );
+//   }
+// }
+
+// class MyStatefulWidget extends StatefulWidget {
+//   const MyStatefulWidget({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+// }
+
+// class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+//   TextEditingController nameController = TextEditingController();
+//   TextEditingController passwordController = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//         padding: const EdgeInsets.all(10),
+//         child: ListView(
+//           children: <Widget>[
+//             Container(
+//                 alignment: Alignment.center,
+//                 padding: const EdgeInsets.all(10),
+//                 child: const Text(
+//                   'TutorialKart',
+//                   style: TextStyle(
+//                       color: Colors.blue,
+//                       fontWeight: FontWeight.w500,
+//                       fontSize: 30),
+//                 )),
+//             Container(
+//                 alignment: Alignment.center,
+//                 padding: const EdgeInsets.all(10),
+//                 child: const Text(
+//                   'Sign in',
+//                   style: TextStyle(fontSize: 20),
+//                 )),
+//             Container(
+//               padding: const EdgeInsets.all(10),
+//               child: TextField(
+//                 controller: nameController,
+//                 decoration: const InputDecoration(
+//                   border: OutlineInputBorder(),
+//                   labelText: 'User Name',
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+//               child: TextField(
+//                 obscureText: true,
+//                 controller: passwordController,
+//                 decoration: const InputDecoration(
+//                   border: OutlineInputBorder(),
+//                   labelText: 'Password',
+//                 ),
+//               ),
+//             ),
+//             TextButton(
+//               onPressed: () {
+//                 //forgot password screen
+//               },
+//               child: const Text(
+//                 'Forgot Password',
+//               ),
+//             ),
+//             Container(
+//                 height: 50,
+//                 padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+//                 child: ElevatedButton(
+//                   child: const Text('Login'),
+//                   onPressed: () {
+//                     print(nameController.text);
+//                     print(passwordController.text);
+//                   },
+//                 )),
+//             Row(
+//               children: <Widget>[
+//                 const Text('Does not have account?'),
+//                 TextButton(
+//                   child: const Text(
+//                     'Sign in',
+//                     style: TextStyle(fontSize: 20),
+//                   ),
+//                   onPressed: () {
+//                     //signup screen
+//                   },
+//                 )
+//               ],
+//               mainAxisAlignment: MainAxisAlignment.center,
+//             ),
+//           ],
+//         ));
+//   }
+// }
+
+
+
+
+
+
+
+
+
+// // import the io version
+// import 'package:openid_client/openid_client_io.dart';
+// // use url launcher package 
+// import 'package:url_launcher/url_launcher.dart';
+
+// authenticate(Uri uri, String clientId, List<String> scopes) async {   
+    
+//     // create the client
+//     var issuer = await Issuer.discover(uri);
+//     var client = new Client(issuer, clientId);
+    
+//     // create a function to open a browser with an url
+//     urlLauncher(String url) async {
+//         if (await canLaunch(url)) {
+//           await launch(url, forceWebView: true);
+//         } else {
+//           throw 'Could not launch $url';
+//         }
+//     }
+    
+
+//     // create an authenticator
+//     var authenticator = new Authenticator(client,
+//         scopes: scopes,
+//         port: 4000, urlLancher: urlLauncher);
+    
+//     // starts the authentication
+//     var c = await authenticator.authorize();
+    
+//     // close the webview when finished
+//     closeWebView();
+    
+//     // return the user info
+//     return await c.getUserInfo();
+
+// }
